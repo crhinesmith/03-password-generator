5a8cdee6ba5df48515acb187bafc96085eb35a9e
ripbog <crhinesmith5@gmail.com>
2022-07-21 19:17:18 -0400

    Mostly if not all finished

--- script.js
+++ script.js
@@ -13,15 +13,17 @@ var special = ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-", ".", "/", ":", 
 
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+
 function generatePassword(){
- 
+  var finalpassword = ""
   var pwlength = window.prompt("Choose password length between 8 and 128 characters");
-  console.log(pwlength);
 
   if (pwlength < 8) {
     window.alert("Please choose password length greater than 8 characters");
+    generatePassword();
   }  if (pwlength > 128) {
     window.alert("Please choose password length less than 128 characters")
+    generatePassword();
   }
  
 
@@ -39,7 +41,22 @@ function generatePassword(){
   var inclNumbers = window.confirm("Do you want to include numbers?");
   console.log(inclNumbers);
 
-
+  for (var i=0; i < pwlength;i++){
+    if (inclLower){
+      finalpassword += letters[Math.floor(Math.random()*letters.length)]
+    }
+    if(inclUpper){
+      finalpassword += lettersCaps[Math.floor(Math.random()*lettersCaps.length)]
+    }
+    if(inclSpecial){
+      finalpassword += special[Math.floor(Math.random()*special.length)]
+    }
+    if(inclNumbers){
+      finalpassword += numbers[Math.floor(Math.random()*numbers.length)]
+    }
+  }
+console.log(finalpassword)
+return finalpassword
 }
 // Write password to the #password input
 function writePassword() {
